56c698201f9ff7942bdaeb6cd068ceb3f922a532
StuartFarmer <email@stuartfarmer.me>
2017-09-26 16:37:35 -0400

    simplified

--- tsol.py
+++ tsol.py
@@ -1,8 +1,9 @@
 # tsol libs
 from solc import compile_source, compile_standard
-from jinja2 import Environment
+from jinja2 import Environment, Template
 from jinja2.nodes import Name
 from io import BytesIO
+import json
 
 BASE_JSON_PAYLOAD = '''{"language": "Solidity", "sources": {
 				"{{name}}": {
@@ -17,26 +18,6 @@ BASE_JSON_PAYLOAD = '''{"language": "Solidity", "sources": {
 				}
 			}
 		}'''
-
-def get_template_variables(fo):
-	nodes = Environment().parse(fo.read()).body[0].nodes
-	var_names = [x.name for x in nodes if type(x) is Name]
-	return var_names
-
-def render_contract(payload):
-	sol_contract = payload.pop('sol')
-	template_variables = get_template_variables(BytesIO(sol_contract.encode()))
-	assert 'contract_name' in payload
-	name = payload.get('contract_name')
-	assert all(x in template_variables for x in list(payload.keys()))
-	template = Environment().from_string(sol_contract)
-	return name, template.render(payload)
-
-def load_tsol_file(path=None, payload=None):
-	assert path and payload, 'No path or payload provided.'
-	payload['sol'] = path.read()
-	name, rendered_contract = render_contract(payload=payload)
-	return name, rendered_contract
 
 def does_compile(payload):
 	try:
@@ -54,3 +35,16 @@ def compilation_payload_from_paths(template, example):
 		payload = json.load(e)
 	solidity = load_tsol_file(code, payload)
 	return Environment().from_string(BASE_JSON_PAYLOAD).render(name=solidity[0], sol=json.dumps(solidity[1]))
+
+def generate_code(template, example):
+	return(Template(template).render(example))
+
+def generate_compilation_payload(template, example):
+	sol = '"{}"'.format(template)
+	name = example['contract_name']
+	base = Template(BASE_JSON_PAYLOAD).render(sol=sol, name=name)
+	code = Template(base).render(example)
+	return(code)
+
+def compile(template, example):
+	return compile_standard(json.loads(generate_compilation_payload(template, example), strict=False))
